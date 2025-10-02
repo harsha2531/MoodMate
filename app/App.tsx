5842d6c62af3a410959eee64f63eb153eb944210
Harsha Nimeda Sirithunga <harshanimeda@gmail.com>
2025-10-03 04:38:22 +0530

    changed configuration files

--- app/App.tsx
+++ app/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import TabNavigator from "./navigation/TabNavigator";
 import { AuthProvider } from "../contexts/AuthContext";
+import "./global.css"
 
 export default function App() {
     return (
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.tsx","./components/**/*.tsx"],
+  // NOTE: Update this to include the paths to all files that contain Nativewind classes.
+  content: ["./App.tsx", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {}
+    extend: {},
   },
-  plugins: []
+  plugins: [],
 }
